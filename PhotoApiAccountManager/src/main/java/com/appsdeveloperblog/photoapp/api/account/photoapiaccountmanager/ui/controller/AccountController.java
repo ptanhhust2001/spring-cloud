4d8ab01b5501b3d3619c5d614483ad4bd91b427b
anhphamdz2k1 <ptanh01230@gmail.com>
2025-04-06 23:32:19 +0700

    create: create gateway

--- PhotoApiAccountManager/src/main/java/com/appsdeveloperblog/photoapp/api/account/photoapiaccountmanager/ui/controller/AccountController.java
+++ PhotoApiAccountManager/src/main/java/com/appsdeveloperblog/photoapp/api/account/photoapiaccountmanager/ui/controller/AccountController.java
@@ -1,15 +1,15 @@
-package com.appdeveloperblog.photoapp.api.user.photoappusers.ui.controller;
+package com.appsdeveloperblog.photoapp.api.account.photoapiaccountmanager.ui.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UsersController {
+@RequestMapping("/account")
+public class AccountController {
 
     @GetMapping("/status/check")
-    public String status() {
-        return "API is up and running!";
+    public String getStatus() {
+        return "Server is up and running";
     }
 }
